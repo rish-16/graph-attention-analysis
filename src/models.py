be2d2cec3128bcb8cc0f6eb88aaff3acfc4f332a
rishabh anand <rish@rishabhs-MacBook-Pro.local>
2022-05-15 13:30:06 +0800

    Add GATv3 model

--- src/models.py
+++ src/models.py
@@ -10,9 +10,21 @@ import igraph
 from igraph import Graph
 import numpy as np
 import matplotlib.pyplot as plt
+from pprint import pprint
 import random
 
 random.seed(0)
+
+def plot_graph(graph):
+    fig, axs = plt.subplots(1, 1)
+    igraph.plot(
+        graph,
+        target=axs,
+        layout="circle",
+        vertex_color="lightblue",
+        vertex_size=15
+    )
+    plt.show()
 
 """
 source code for the GAT v3
@@ -37,8 +49,20 @@ class Eigen(nn.Module):
     def forward(self, edge_idx):
         lap_idx, lap_wt = get_laplacian(edge_idx, normalization="sym")
         lap_adj = to_dense_adj(lap_idx)
-        top_eig = torch.linalg.eig(lap_adj)[1:self.k+1, :]
-        return top_eig
+        eigenvals, eigenvecs = torch.linalg.eig(lap_adj)
+        top_eig = eigenvecs.squeeze(0)[:, 1:self.k+1]
+        top_eig = torch.real(top_eig)
+        new_edge_features = torch.Tensor(edge_idx.size(1), 2 * self.k)
+        new_edge_idx = edge_idx.T
+
+        for idx, pair in enumerate(new_edge_idx):
+            i, j = pair
+            x_i_prime = top_eig[i]
+            x_j_prime = top_eig[j]
+            new_feat = torch.cat([x_i_prime, x_j_prime], dim=0)
+            new_edge_features[idx] = new_feat
+
+        return new_edge_features
 
 class GATv3Layer(tgnn.MessagePassing):
     def __init__(self, indim, eigendim, outdim):
@@ -79,28 +103,42 @@ class GATv3Layer(tgnn.MessagePassing):
         
         return msg
 
-# model = GATv3Layer(256, 64, 10)
-# edge_idx = torch.randint(0, 10, (2, 200)).long()
-# edge_attr = torch.rand(200, 64)
-# x = torch.rand(100, 256)
-# y = model(x, edge_attr, edge_idx)
-# print (y.shape)
+class GATv3(nn.Module):
+    def __init__(self, indim, eigendim, hidden, outdim, k):
+        super().__init__()
 
-graph = Graph.Erdos_Renyi(n=15, p=0.2, directed=False, loops=False)
+        self.eigen = Eigen(k)
+        self.gat1 = GATv3Layer(indim, eigendim, hidden)
+        self.gat2 = GATv3Layer(hidden, eigendim, outdim)
 
-# fig, axs = plt.subplots(1, 1)
-# igraph.plot(
-#     graph,
-#     target=axs,
-#     layout="circle",
-#     vertex_color="lightblue",
-#     vertex_size=15
-# )
-# plt.show()
+    def forward(self, x, edge_idx, edge_attr):
+        with torch.no_grad():
+            eigen_x = self.eigen(edge_idx)
+        x = torch.relu(self.gat1(x, eigen_x, edge_idx))
+        out = torch.softmax(self.gat2(x, eigen_x, edge_idx), 1)
 
-adj = torch.from_numpy(np.array(list(graph.get_adjacency())))
-edge_idx = tg.utils.dense_to_sparse(adj)
-print (edge_idx)
-eigen = Eigen(4)
-eig = eigen(edge_idx)
-print (eig.shape)
+        return out
+
+# graph = Graph.Erdos_Renyi(n=15, p=0.2, directed=False, loops=False)
+# adj = torch.from_numpy(np.array(list(graph.get_adjacency())))
+# edge_idx, _ = tg.utils.dense_to_sparse(adj)
+# n_edges = graph.ecount()
+# edge_attr = torch.rand(n_edges, 64)
+# x = torch.rand(15, 128)
+# y = torch.rand(size=(15, 1))
+# y[y > 0.5] = 1
+# y[y <= 0.5] = 0
+
+# gat = GATv3(128, 8, 32, 10, 4)
+# criterion = nn.CrossEntropyLoss()
+# optim = torch.optim.Adam(gat.parameters())
+
+# for i in range(10):
+#     optim.zero_grad()
+#     pred = gat(x, edge_idx, edge_attr).unsqueeze(0)
+#     print (pred.shape)
+#     loss = criterion(pred, y.unsqueeze(0))
+#     loss.backward()
+#     optim.step()
+
+#     print (loss)
